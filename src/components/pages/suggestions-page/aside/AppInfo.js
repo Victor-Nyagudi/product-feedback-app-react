b64828528602897d7577ad8c81b4731e6c248c0c
Victor Nyagudi <nyagudivicktor@gmail.com>
2022-12-10 11:31:40 +0300

    Add hamburger button to AppInfo.js for mobile layout

--- src/components/pages/suggestions-page/aside/AppInfo.js
+++ src/components/pages/suggestions-page/aside/AppInfo.js
@@ -1,13 +1,21 @@
 function AppInfo() {
     return ( 
         <section className="app-info">
-            <h1 className="app-info__title">
-                Frontend Mentor
-            </h1>
-            
-            <p className="app-info__sub-heading">
-                Feedback Board
-            </p>
+            <div className="app-info__container">
+                <h1 className="app-info__title">
+                    Frontend Mentor
+                </h1>
+                
+                <p className="app-info__sub-heading">
+                    Feedback Board
+                </p>
+            </div>
+
+            <div className="app-info__container">
+                <button className="app-info__hamburger-btn">
+                    <img src="" alt="" className="app-info__hamburger-img" aria-hidden="true" />
+                </button>
+            </div>
         </section>
     );
 }
