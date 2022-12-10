43564b0a3d73a06fe7efbb7457ab3fcece82996c
Victor Nyagudi <nyagudivicktor@gmail.com>
2022-12-10 11:44:05 +0300

    Add mobile layout components to SuggestionsPage.js

--- src/components/pages/suggestions-page/SuggestionsPage.js
+++ src/components/pages/suggestions-page/SuggestionsPage.js
@@ -1,7 +1,19 @@
+import AppInfo from "./aside/AppInfo";
+import DesktopAside from "./aside/DesktopAside";
+import SuggestionsContent from "./main/SuggestionsContent";
+
 function SuggestionsPage() {
     return ( 
         <div className="suggestions container">
-            
+            {/* Mobile laayout */}
+            <AppInfo />
+
+            {/* 
+                Render on desktop/tablets
+                <DesktopAside /> 
+            */}
+
+            <SuggestionsContent />
         </div>
     );
 }
