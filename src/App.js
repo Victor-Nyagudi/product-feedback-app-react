11b9c2751aec6a8f619426a659da08f18787d1cf
Victor Nyagudi <nyagudivicktor@gmail.com>
2022-12-10 12:34:44 +0300

    Add resize event listeners to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
+import SuggestionsPage from './components/pages/suggestions-page/SuggestionsPage';
 import './css/main.css';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+  const mobileLayoutUpperBound = 600;
+  const tabletLayoutUpperBound = 820;
+
+  const [isMobileScreen, setIsMobileScreen] = useState(window.outerWidth < mobileLayoutUpperBound);
+  const [isTabletScreen, setIsTabletScreen] = 
+      useState(window.outerWidth > mobileLayoutUpperBound && window.outerWidth < tabletLayoutUpperBound);
+  
+  /* 
+    ? Listening for resize event for styling purposes. 
+    ? This way, the page is re-rendered as I change the browser size using
+    ? Chrome developer tools to see at what point the layout starts to break.
+    ! Without this, the page isn't re-rendered while adjusting screen size.
+  */ 
+  useEffect(() => {
+    window.addEventListener('resize', showMobileLayout);
+
+    return () => window.removeEventListener('resize', showMobileLayout);
+  }, [isMobileScreen]);
+
+  /* 
+    * The behavior is interesting at the 600 and 820 breakpoint.
+    * Log 'isTabletScreen' to the console to see it. Works fine though.
+  */
+  useEffect(() => {
+    window.addEventListener('resize', showTabletLayout);
+
+    return () => window.addEventListener('resize', showTabletLayout);
+  }, [isTabletScreen]);
+  
+
+  function showTabletLayout() {
+    setIsTabletScreen(window.outerWidth > mobileLayoutUpperBound && window.outerWidth < tabletLayoutUpperBound);
+  }
+
+  function showMobileLayout() {
+    setIsMobileScreen(window.outerWidth < mobileLayoutUpperBound);
+  }
+
   return (
-    <h1>And so it begins...</h1>
+    <>
+      <SuggestionsPage />
+    </>
   );
 }
 
