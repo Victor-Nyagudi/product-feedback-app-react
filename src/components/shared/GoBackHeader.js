d88b3b6178a8ac9077e1daebf1f36acf8ecdc8a6
Victor Nyagudi <nyagudivicktor@gmail.com>
2022-12-12 10:27:22 +0300

    Add GoBackHeader.js component

---
+++ src/components/shared/GoBackHeader.js
@@ -0,0 +1,33 @@
+import Button from "./Button";
+
+function GoBackHeader({ hasSecondaryButton, secButtonText, secButtonClassName, secButtonIcon }) {
+    return ( 
+        <div className="go-back-header">
+            <div className="go-back__primary-container">
+                <button className="go-back__primary-button button">
+                    <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M6 9L2 5l4-4" stroke="#4661E6" strokeWidth="2" fill="none" fillRule="evenodd"/>
+                    </svg>
+
+                    <span className="go-back__primary-button-text">
+                        Go Back
+                    </span>
+                </button>
+            </div>
+            
+            {
+                hasSecondaryButton &&
+
+                <div className="go-back__secondary-container">
+                    <Button 
+                        text={ secButtonText }
+                        icon={ secButtonIcon }
+                        className={ secButtonClassName }
+                    />
+                </div>
+            }
+        </div>
+    );
+}
+
+export default GoBackHeader;
