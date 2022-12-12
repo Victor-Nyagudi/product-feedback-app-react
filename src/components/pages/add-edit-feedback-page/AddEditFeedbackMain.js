991b0a74093668f4007f336d5d6a285883c83424
Victor Nyagudi <nyagudivicktor@gmail.com>
2022-12-12 10:54:37 +0300

    Add AddEditFeedbackMain.js component

---
+++ src/components/pages/add-edit-feedback-page/AddEditFeedbackMain.js
@@ -0,0 +1,15 @@
+function AddEditFeedbackMain({ title }) {
+    return ( 
+        <main className="add-edit-feedback">
+            <h1 className="add-edit-feedback__title">
+                { title }
+            </h1>
+
+            <form className="add-edit-feedback__form">
+
+            </form>
+        </main>
+    );
+}
+
+export default AddEditFeedbackMain;
