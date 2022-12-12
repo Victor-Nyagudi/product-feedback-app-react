bab0874ec59ea642d24dc9282acd491ae79ebb0e
Victor Nyagudi <nyagudivicktor@gmail.com>
2022-12-12 09:21:46 +0300

    Wrap Tag text inside a button SuggestionsPage

--- src/components/pages/suggestions-page/aside/Tag.js
+++ src/components/pages/suggestions-page/aside/Tag.js
@@ -1,7 +1,9 @@
 function Tag({ tagName, isSelected }) {
     return ( 
         <li className={ isSelected ? "tags__tag tag--active" : "tags__tag" }>
-            { tagName }
+            <button className="tags__tag-button button">
+                { tagName }
+            </button>
         </li>
     );
 }
