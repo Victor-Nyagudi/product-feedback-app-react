321a1f300d5f6ad5318b5091e2b80bd85f99e37f
Victor Nyagudi <nyagudivicktor@gmail.com>
2022-12-14 11:03:34 +0300

    Style feedback item of FeedbackDetailPage mobile

--- src/components/shared/FeedbackItem.js
+++ src/components/shared/FeedbackItem.js
@@ -20,7 +20,7 @@ function FeedbackItem() {
                     </svg>
                 </div>
 
-                <p className="feedback-item__total-upvotes">
+                <p className="feedback-item__upvotes-total">
                     99
                 </p>
             </div>
@@ -30,13 +30,13 @@ function FeedbackItem() {
                     <path d="M2.62 16H1.346l.902-.91c.486-.491.79-1.13.872-1.823C1.036 11.887 0 9.89 0 7.794 0 3.928 3.52 0 9.03 0 14.87 0 18 3.615 18 7.455c0 3.866-3.164 7.478-8.97 7.478-1.017 0-2.078-.137-3.025-.388A4.705 4.705 0 012.62 16z" fill="#CDD2EE" fillRule="nonzero"/>
                 </svg>
 
-                 <p className="feedback-item__total-comments">
+                 <p className="feedback-item__comments-total">
                     4
                  </p>
             </div>
             
             <div className="feedback-item__tag-container">
-                <div className="feedbak-message__tag">
+                <div className="feedback-item__tag">
                     <p className="feedback-item__tag-text">
                         Feature
                     </p>
