e705627bb4e3b9af2af61cce2c00540d06f7a23f
Victor Nyagudi <nyagudivicktor@gmail.com>
2022-12-18 16:51:28 +0300

    Use a button element for upvotes button instead of div
    This is for the feedback item in FeedbackDetailpage and RoadmapPage

--- src/components/shared/FeedbackItem.js
+++ src/components/shared/FeedbackItem.js
@@ -31,11 +31,11 @@ function FeedbackItem({ showBadge = false, badgeText, badgeColor }) {
                         <svg width="10" height="7" xmlns="http://www.w3.org/2000/svg">
                             <path d="M1 6l4-4 4 4" stroke="#4661E6" strokeWidth="2" fill="none" fillRule="evenodd"/>
                         </svg>
-                    </button>
 
-                    <p className="feedback-content__upvotes-total">
-                        99
-                    </p>
+                        <span className="feedback-content__upvotes-total">
+                            99
+                        </span>
+                    </button>
                 </div>
                 
                 <div className="feedback-content__comments">
