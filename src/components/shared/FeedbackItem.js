a4812c7449039cb3d8859f481ba8c9d31349b0c2
Victor Nyagudi <nyagudivicktor@gmail.com>
2022-12-18 11:19:00 +0300

    Use a button element for the feedback upvote button RoadmapPage

--- src/components/shared/FeedbackItem.js
+++ src/components/shared/FeedbackItem.js
@@ -27,11 +27,11 @@ function FeedbackItem({ showBadge = false, badgeText, badgeColor }) {
                 </div>
 
                 <div className="feedback-content__upvotes">
-                    <div className="feedback-content__img-container">
+                    <button type="button" className="feedback-content__upvotes-button button">
                         <svg width="10" height="7" xmlns="http://www.w3.org/2000/svg">
                             <path d="M1 6l4-4 4 4" stroke="#4661E6" strokeWidth="2" fill="none" fillRule="evenodd"/>
                         </svg>
-                    </div>
+                    </button>
 
                     <p className="feedback-content__upvotes-total">
                         99
