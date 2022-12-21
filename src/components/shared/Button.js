1c9f6b20b3872b63abbc75afca9e6be1670a842e
Victor Nyagudi <nyagudivicktor@gmail.com>
2022-12-21 10:17:53 +0300

    Add comment explaining class names in Button.js

--- src/components/shared/Button.js
+++ src/components/shared/Button.js
@@ -3,11 +3,17 @@ import { PropTypes } from "prop-types";
 function Button({ text, className, icon, type }) {
     return ( 
         /*
-            ? These are the options for each button type's className
+            ? These are the options for each button className
 
             * Purple - 'button--add-feedback'
             * Blue - 'button--edit-feedback'
             * Black - 'button--cancel'
+            
+            ? I understand I should've given them more meaningful names
+            ? like 'button--purple', but I missed the fact that the text
+            ? inside similarly-colored buttons could differ depending
+            ? on the page in the design. This is tentatively something
+            ? that could be changed in the future.
         */
         <button 
             type={ `${type}` }
