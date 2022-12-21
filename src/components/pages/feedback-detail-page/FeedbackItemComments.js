85c75ab8dfa9f4def997fad9dbefb3c60101c92d
Victor Nyagudi <nyagudivicktor@gmail.com>
2022-12-22 00:24:47 +0300

    Update comments section heading FeedbackDetailPage
    The heading now switches between singular and plural form depending on if there's one or more comments

--- src/components/pages/feedback-detail-page/FeedbackItemComments.js
+++ src/components/pages/feedback-detail-page/FeedbackItemComments.js
@@ -3,12 +3,14 @@ import { PropTypes } from "prop-types";
 import FeedbackItemComment from "./FeedbackItemComment";
 
 function FeedbackItemComments({ comments }) {
+    const headingText = comments.length > 1 ? 'Comments' : 'Comment';
+
     return ( 
         <section className="feedback-detail__comments-container">
             <h2 className="feedback-detail__comments-title">
                 <span className="feedback-detail__total-comments">
                     { comments.length }
-                </span> Comments
+                </span> { headingText }
             </h2>
 
             <ul className="feedback-detail__comments">
