d6cdebd404e633f58323b83b4b65897e4f050aaa
Victor Nyagudi <nyagudivicktor@gmail.com>
2022-12-22 15:27:48 +0300

    Clicking the go back button now takes users to previous page
    This works in all pages with a go back button

--- src/components/shared/GoBackButton.js
+++ src/components/shared/GoBackButton.js
@@ -1,8 +1,19 @@
 import { PropTypes } from "prop-types";
+import { useNavigate } from "react-router-dom";
 
 function GoBackButton({ iconColor }) {
+    /*
+        * Passing -1 to navigate() is equivalent to hitting the back button
+        * next to the browser URL. Read more about it here:
+        * https://reactrouter.com/en/main/hooks/use-navigate
+    */
+    const navigate = useNavigate();
+    
     return (    
-        <button className="go-back__primary-button button">
+        <button 
+            className="go-back__primary-button button"
+            onClick={ () => navigate(-1) }
+        >
             <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
                 <path d="M6 9L2 5l4-4" stroke={ iconColor } strokeWidth="2" fill="none" fillRule="evenodd"/>
             </svg>
--- src/components/shared/GoBackHeader.js
+++ src/components/shared/GoBackHeader.js
@@ -1,19 +1,12 @@
 import Button from "./Button";
 import { PropTypes } from "prop-types";
+import GoBackButton from "./GoBackButton";
 
 function GoBackHeader({ hasSecondaryButton, secButtonText, secButtonClassName, secButtonIcon }) {
     return ( 
         <div className={ hasSecondaryButton ? "go-back-header--secondary" : "go-back-header"}>
             <div className="go-back__primary-container">
-                <button className="go-back__primary-button button">
-                    <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M6 9L2 5l4-4" stroke="#4661E6" strokeWidth="2" fill="none" fillRule="evenodd"/>
-                    </svg>
-
-                    <span className="go-back__primary-button-text">
-                        Go Back
-                    </span>
-                </button>
+                <GoBackButton />
             </div>
             
             {
