91203e72e37b519051097286f51f7f6db3145392
Victor Nyagudi <nyagudivicktor@gmail.com>
2022-12-25 08:02:56 +0300

    Switch to feedback items from db.json in RoadmapPage

--- src/components/pages/roadmap-page/RoadmapPage.js
+++ src/components/pages/roadmap-page/RoadmapPage.js
@@ -21,14 +21,14 @@ function RoadMapPage({ sharedProps }) {
                 
                 <RoadmapMobileNavbar 
                     getActiveMobileNavItem={ getActiveMobileNavItem }
-                    feedbackItems={ sharedProps.feedbackItems }
+                    feedbackItems={ sharedProps.dbFeedbackItems }
                 />
             }
 
             <RoadmapMain 
                 isSmallerThan700px={ sharedProps.isSmallerThan700px } 
                 activeMobileNavItem={ activeMobileNavItem }
-                feedbackItems={ sharedProps.feedbackItems }
+                feedbackItems={ sharedProps.dbFeedbackItems }
                 getSelectedFeedbackItemId={ sharedProps.showFeedbackItemDetail }
             />
         </div>
