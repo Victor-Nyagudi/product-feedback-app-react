eaeaf93a07076d0020bd3fe62901c89648cc6d2e
Victor Nyagudi <nyagudivicktor@gmail.com>
2022-12-28 10:02:22 +0300

    Clicking hamburger button now toggles icon

--- src/components/pages/suggestions-page/aside/AppInfo.js
+++ src/components/pages/suggestions-page/aside/AppInfo.js
@@ -1,6 +1,9 @@
 import { PropTypes } from "prop-types";
+import React, { useState } from 'react';
 
 function AppInfo({ isMobileScreen }) {
+    const [shouldShowHamburgerButton, setShouldShowHamburgerButton] = useState(true);
+
     return ( 
         <section className="app-info">
             <div className="app-info__content container">
@@ -17,10 +20,24 @@ function AppInfo({ isMobileScreen }) {
                 {
                     isMobileScreen &&
 
-                    <button className="app-info__hamburger-button button" aria-label="Open menu">
-                        <svg width="20" height="17" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                            <g fill="#FFF" fillRule="evenodd"><path d="M0 0h20v3H0zM0 7h20v3H0zM0 14h20v3H0z"/></g>
-                        </svg>
+                    <button 
+                        className="app-info__hamburger-button button" 
+                        aria-label="Open menu"
+                        onClick={ () => setShouldShowHamburgerButton(!shouldShowHamburgerButton) }
+                    >
+                        {
+                            shouldShowHamburgerButton ?
+                            
+                            <svg width="20" height="17" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                <g fill="#FFF" fillRule="evenodd"><path d="M0 0h20v3H0zM0 7h20v3H0zM0 14h20v3H0z"/></g>
+                            </svg>
+
+                            :
+
+                            <svg width="18" height="17" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                <path d="M15.01.368l2.122 2.122-6.01 6.01 6.01 6.01-2.122 2.122L9 10.622l-6.01 6.01L.868 14.51 6.88 8.5.87 2.49 2.988.368 9 6.38 15.01.37z" fill="#FFF" fillRule="evenodd"/>
+                            </svg>
+                        }
                     </button>
                 }
             </div>
