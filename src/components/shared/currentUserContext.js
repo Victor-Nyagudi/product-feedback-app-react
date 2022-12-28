16538e9ab4cc4c1925753a2d07b054e0acae4586
Victor Nyagudi <nyagudivicktor@gmail.com>
2022-12-28 14:04:09 +0300

    Add currentUserContext.js - previous commit didn't include it

---
+++ src/components/shared/currentUserContext.js
@@ -0,0 +1,14 @@
+/*
+    * This medium helped in setting up context for function components
+    * https://medium.com/@danfyfe/using-react-context-with-functional-components-153cbd9ba214
+    
+    * React official docs on Context that helps prevent prop drilling the current User
+    * https://reactjs.org/docs/context.html#caveats
+    * Note the use of camel case in naming this file since it's not a component
+*/
+
+import React from 'react';
+
+const CurrentUserContext = React.createContext({});
+
+export default  CurrentUserContext;
