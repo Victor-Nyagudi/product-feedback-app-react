a09c25406d4d7537b268ff24999b939f16062fa7
Victor Nyagudi <nyagudivicktor@gmail.com>
2023-02-23 23:24:20 +0300

    Add HasBadge story to Feedback Item
    Commented out page.css because it was interefering with FeedbackItem.stories.js i.e. using its styling instead of the one imported in .storybook/preview.js

--- src/stories/project-stories/FeedbackItem.stories.js
+++ src/stories/project-stories/FeedbackItem.stories.js
@@ -17,8 +17,20 @@ export const NoBadge = Template.bind({});
 NoBadge.args = {
     title: "Improve contrast ratio of buttons",
     message: "The contrast between the button text and the background isn't very good. It's hard to read the text at a glance, so maybe switching up the colors would help make things more readable",
-    category: "UX",
+    tagCategory: "UX",
     totalUpvotes: 15,
     totalComments: 3,
+}
 
+export const HasBadge = Template.bind({});
+
+HasBadge.args = {
+    title: "Add dark mode",
+    message: "Much easier on the eyes, especially at night after a long day of looking at screens all the time",
+    totalUpvotes: 123,
+    totalComments: 7,
+    showBadge: true,
+    badgeText: "Planned",
+    badgeColor: "light-blue",
+    tagCategory: "Feature"
 }
