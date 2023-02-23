ad76fbb66665bdcdc9d98130c8a74ede7e71d3bb
Victor Nyagudi <nyagudivicktor@gmail.com>
2023-02-23 12:30:25 +0300

    Rename Button and its story to SampleButton

--- src/stories/Header.jsx
+++ src/stories/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Button } from './Button';
+import { SampleButton } from './SampleButton';
 import './header.css';
 
 export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
@@ -32,12 +32,12 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
             <span className="welcome">
               Welcome, <b>{user.name}</b>!
             </span>
-            <Button size="small" onClick={onLogout} label="Log out" />
+            <SampleButton size="small" onClick={onLogout} label="Log out" />
           </>
         ) : (
           <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
+            <SampleButton size="small" onClick={onLogin} label="Log in" />
+            <SampleButton primary size="small" onClick={onCreateAccount} label="Sign up" />
           </>
         )}
       </div>
