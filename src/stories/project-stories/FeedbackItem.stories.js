c825d1dc5dbf25a2939cd0ceb5ddb0db5f51f0cb
Victor Nyagudi <nyagudivicktor@gmail.com>
2023-02-24 10:40:26 +0300

    Add a decorator to FeedbackItem's stories

--- src/stories/project-stories/FeedbackItem.stories.js
+++ src/stories/project-stories/FeedbackItem.stories.js
@@ -5,9 +5,13 @@ import FeedbackItem from '../../components/shared/FeedbackItem';
 export default {
     title: "Project/Shared/FeedbackItem",
     component: FeedbackItem,
-    parameters: {
-        backgrounds: { disable: true }
-    }
+    decorators: [
+        (Story) => (
+            <div className="suggestions__main">
+                <Story />
+            </div>
+        )
+    ]
 }
 
 const Template = (args) => <FeedbackItem {...args} />;
@@ -15,6 +19,7 @@ const Template = (args) => <FeedbackItem {...args} />;
 export const NoBadge = Template.bind({});
 
 NoBadge.args = {
+    id: 1,
     title: "Improve contrast ratio of buttons",
     message: "The contrast between the button text and the background isn't very good. It's hard to read the text at a glance, so maybe switching up the colors would help make things more readable",
     tagCategory: "UX",
@@ -25,12 +30,13 @@ NoBadge.args = {
 export const HasBadge = Template.bind({});
 
 HasBadge.args = {
+    id: 2,
     title: "Add dark mode",
     message: "Much easier on the eyes, especially at night after a long day of looking at screens all the time",
     totalUpvotes: 123,
     totalComments: 7,
     showBadge: true,
-    badgeText: "Planned",
+    badgeText: "Live",
     badgeColor: "light-blue",
     tagCategory: "Feature"
 }
