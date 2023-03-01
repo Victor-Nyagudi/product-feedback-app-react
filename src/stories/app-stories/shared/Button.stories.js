a40be46b638247801a57a4f0cf6ece3fa3541b97
Victor Nyagudi <nyagudivicktor@gmail.com>
2023-03-01 23:11:51 +0300

    Add stories for shared Button
    This button potentially uses React Router

--- src/stories/app-stories/shared/Button.stories.js
+++ src/stories/app-stories/shared/Button.stories.js
@@ -1,17 +1,55 @@
 import React from 'react';
+import { BrowserRouter } from "react-router-dom";
 
 import Button  from "../../../components/shared/Button";
 
 export default {
     title: "App/Shared/Button",
-    component: Button
+    component: Button,
+    decorators: [
+        (Story) => (
+            <BrowserRouter>
+                <Story />
+            </BrowserRouter>
+        )
+    ],
+    args: {
+        isLink: false,
+        type: "button"        
+    }     
 }
 
 const Template = (args) => <Button {...args} />
 
-export const Primary = Template.bind({});
+export const Purple = Template.bind({});
+export const PurpleLink = Template.bind({});
 
-Primary.args = {
+export const Blue = Template.bind({});
+export const Black = Template.bind({});
+export const Orange = Template.bind({});
+
+Purple.args = {
     text: "Add feedback",
-    className: "button--add-feedback"
+    className: "button--add-feedback",
+}
+
+PurpleLink.args = {
+    ...Purple.args,
+    isLink: true, 
+    icon: <svg width="9" height="9" xmlns="http://www.w3.org/2000/svg" className="feedback-button-svg"><text transform="translate(-24 -20)" fill="#F2F4FE" fillRule="evenodd" fontFamily="Jost-Bold, Jost" fontSize="14" fontWeight="bold"><tspan x="24" y="27.5">+</tspan></text></svg>
+}
+
+Blue.args = {
+    text: "Edit feedback",
+    className: "button--edit-feedback"
+}
+
+Black.args = {
+    text: "Cancel",
+    className: "button--cancel"
+}
+
+Orange.args = {
+    text: "Delete",
+    className: "button--delete"
 }
