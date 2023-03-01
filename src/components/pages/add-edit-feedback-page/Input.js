36b6cfad6f335795b90a8039dd21aa0455c7d9c8
Victor Nyagudi <nyagudivicktor@gmail.com>
2023-03-01 15:39:18 +0300

    Add aria-label to dropdown button

--- src/components/pages/add-edit-feedback-page/Input.js
+++ src/components/pages/add-edit-feedback-page/Input.js
@@ -103,6 +103,7 @@ function Input({
                         type="button" 
                         className="input__dropdown-toggle button"
                         onClick={ () => toggleDropdownMenu(false) }
+                        aria-label="Open dropdown"
                     >
                         <svg width="10" height="7" xmlns="http://www.w3.org/2000/svg">
                             <path d="M1 1l4 4 4-4" stroke="#4661E6" strokeWidth="2" fill="none" fillRule="evenodd"/>
