fd067a38b9d8a4e462ca3a94072342454771cf81
Victor Nyagudi <nyagudivicktor@gmail.com>
2023-03-05 16:59:30 +0300

    Add interactions tests to Appinfo stories

--- src/components/pages/suggestions-page/aside/AppInfo.js
+++ src/components/pages/suggestions-page/aside/AppInfo.js
@@ -34,13 +34,13 @@ function AppInfo({ isMobileScreen, showHideSideMenu }) {
                         {
                             shouldShowHamburgerButton ?
                             
-                            <svg width="20" height="17" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <svg width="20" height="17" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" data-testid="hamburger-button">
                                 <g fill="#FFF" fillRule="evenodd"><path d="M0 0h20v3H0zM0 7h20v3H0zM0 14h20v3H0z"/></g>
                             </svg>
 
                             :
 
-                            <svg width="18" height="17" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <svg width="18" height="17" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" data-testid="close-menu-button">
                                 <path d="M15.01.368l2.122 2.122-6.01 6.01 6.01 6.01-2.122 2.122L9 10.622l-6.01 6.01L.868 14.51 6.88 8.5.87 2.49 2.988.368 9 6.38 15.01.37z" fill="#FFF" fillRule="evenodd"/>
                             </svg>
                         }
--- src/stories/app-stories/pages/suggestions-page/aside/AppInfo.stories.js
+++ src/stories/app-stories/pages/suggestions-page/aside/AppInfo.stories.js
@@ -2,13 +2,21 @@ import React from "react";
 
 import AppInfo from "../../../../../components/pages/suggestions-page/aside/AppInfo";
 
+import { within, userEvent } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
+
 export default {
     title: "App/Pages/Suggestions/Aside/AppInfo",
     component: AppInfo,
+    argTypes: {
+        showHideSideMenu: { action: "showHideSideMenu" }
+    },
     decorators: [
         (Story) => (
             <div className="suggestions">
-                <Story />
+                <aside className="suggestions__aside">
+                    <Story />
+                </aside>
             </div>
         )
     ]
@@ -16,13 +24,23 @@ export default {
 
 const Template = (args) => <AppInfo {...args} />;
 
-export const Desktop = Template.bind({});
-export const Mobile = Template.bind({});
+export const NoMenuButton = Template.bind({});
+export const MobileWithMenuButton = Template.bind({});
 
-Desktop.args = {
-    isMobileScreen: false
-};
+NoMenuButton.args = { isMobileScreen: false };
 
-Mobile.args = {
-    isMobileScreen: true
+MobileWithMenuButton.args = { isMobileScreen: true };
+
+MobileWithMenuButton.play = async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await userEvent.click(canvas.getByRole("button"));
+
+    await expect(canvas.getByTestId("close-menu-button")).toBeInTheDocument();
+    await expect(canvas.queryByTestId("hamburger-button")).not.toBeInTheDocument();
+
+    await userEvent.click(canvas.getByRole("button"));
+
+    await expect(canvas.getByTestId("hamburger-button")).toBeInTheDocument();
+    await expect(canvas.queryByTestId("close-menu-button")).not.toBeInTheDocument();
 }
