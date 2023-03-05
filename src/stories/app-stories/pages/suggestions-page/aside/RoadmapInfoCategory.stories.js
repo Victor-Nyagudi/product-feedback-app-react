449cccdd2e0c1f4b0dd01a5f1953482fc9f72c3d
Victor Nyagudi <nyagudivicktor@gmail.com>
2023-03-05 17:45:56 +0300

    Remove unnecessary stories

--- src/stories/app-stories/pages/suggestions-page/aside/RoadmapInfoCategory.stories.js
+++ src/stories/app-stories/pages/suggestions-page/aside/RoadmapInfoCategory.stories.js
@@ -4,7 +4,14 @@ import RoadmapInfoCategory from "../../../../../components/pages/suggestions-pag
 
 export default {
     title: "App/Pages/Suggestions/Aside/RoadmapInfoCategory",
-    component: RoadmapInfoCategory
+    component: RoadmapInfoCategory,
+    decorators: [
+        (Story) => (
+            <aside className="suggestions__aside">
+                <Story />
+            </aside>
+        )
+    ]
 }
 
 const Template = (args) => <RoadmapInfoCategory {...args} />;
@@ -13,10 +20,6 @@ export const Planned = Template.bind({});
 export const InProgress = Template.bind({});
 export const Live = Template.bind({});
 
-export const PlannedWrappedByAside = Template.bind({});
-export const InProgressWrappedByAside = Template.bind({});
-export const LiveWrappedByAside = Template.bind({});
-
 
 Planned.args = {
     category: "Planned",
@@ -24,46 +27,14 @@ Planned.args = {
     iconColor: "orange"
 };
 
-PlannedWrappedByAside.decorators = [
-    (Story) => (
-        <aside className="suggestions__aside">
-            <Story />
-        </aside>
-    )
-];
-
-PlannedWrappedByAside.args = { ...Planned.args }
-
-
 InProgress.args = {
     category: "In Progress",
     quantity: 4,
     iconColor: "purple"
 };
 
-InProgressWrappedByAside.decorators = [
-    (Story) => (
-        <aside className="suggestions__aside">
-            <Story />
-        </aside>
-    )
-];
-
-InProgressWrappedByAside.args = { ...InProgress.args }
-
-
 Live.args = {
     category: "Live",
     quantity: 6,
     iconColor: "light-blue"
 }
-
-LiveWrappedByAside.decorators = [
-    (Story) => (
-        <aside className="suggestions__aside">
-            <Story />
-        </aside>
-    )
-];
-
-LiveWrappedByAside.args = { ...Live.args }
