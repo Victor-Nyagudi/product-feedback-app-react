9a222146d40d003ffbca5a3abbf060c58ce9356c
Victor Nyagudi <nyagudivicktor@gmail.com>
2023-03-13 07:31:21 +0300

    Replace JSON object with plain JavaScript object

--- src/stories/app-stories/shared/FeedbackItem.stories.js
+++ src/stories/app-stories/shared/FeedbackItem.stories.js
@@ -10,29 +10,29 @@ export default {
     component: FeedbackItem,
     args: {
         feedbackItemObject: {
-            "id": 1,
-            "title": "Add tags for solutions",
-            "category": "Enhancement",
-            "upvotes": 112,
-            "status": "suggestion",
-            "description": "Easier to search for solutions based on a specific stack.",
-            "comments": [
+            id: 1,
+            title: "Add tags for solutions",
+            category: "Enhancement",
+            upvotes: 112,
+            status: "suggestion",
+            description: "Easier to search for solutions based on a specific stack.",
+            comments: [
                 {
-                    "id": 1,
-                    "content": "Awesome idea! Trying to find framework-specific projects within the hubs can be tedious",
-                    "user": {
-                        "image": "./assets/user-images/image-suzanne.jpg",
-                        "name": "Suzanne Chang",
-                        "username": "upbeat1811"
+                    id: 1,
+                    content: "Awesome idea! Trying to find framework-specific projects within the hubs can be tedious",
+                    user: {
+                        image: "./assets/user-images/image-suzanne.jpg",
+                        name: "Suzanne Chang",
+                        username: "upbeat1811"
                     }
                 },
                 {
-                    "id": 2,
-                    "content": "Please use fun, color-coded labels to easily identify them at a glance",
-                    "user": {
-                        "image": "./assets/user-images/image-thomas.jpg",
-                        "name": "Thomas Hood",
-                        "username": "brawnybrave"
+                    id: 2,
+                    content: "Please use fun, color-coded labels to easily identify them at a glance",
+                    user: {
+                        image: "./assets/user-images/image-thomas.jpg",
+                        name: "Thomas Hood",
+                        username: "brawnybrave"
                     }
                 }
             ]
