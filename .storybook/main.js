d43b2552fea5dc5b27c85e28945927fbc77882f2
Victor Nyagudi <nyagudivicktor@gmail.com>
2023-03-13 08:46:02 +0300

    Add coverage add-on

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,6 +8,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
+    "@storybook/addon-coverage",
     "@storybook/preset-create-react-app"
   ],
   "features": {
